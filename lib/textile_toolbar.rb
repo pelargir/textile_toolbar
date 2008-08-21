d11a5e8557bfa3e0bc1371541292a4648716f36a
Matthew Bass <matt@anacreon.local>
2008-08-21 15:49:25 -0400

    added textile enabled link

--- lib/textile_toolbar.rb
+++ lib/textile_toolbar.rb
@@ -3,6 +3,8 @@ module TextileToolbar
     html = link_to_function(image_tag("textile_toolbar/bold.png", :size => "23x22", :alt => "Make selection bold"), "surround_selection('#{id}', '*')")
     html << "&nbsp;"
     html << link_to_function(image_tag("textile_toolbar/italic.png", :size => "23x22", :alt => "Make selection italic"), "surround_selection('#{id}', '_')")
+    html << "&nbsp;"
+    html << link_to("Textile", "http://hobix.com/textile/") << " enabled"
   end
 end
 
