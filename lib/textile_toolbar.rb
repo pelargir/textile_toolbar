fea34a2ed11645cbcaac8669935e44026350783d
Matthew Bass <matt@70-11-217-178.area7.spcsdns.net>
2008-08-21 18:07:53 -0400

    added hyperlink button

--- lib/textile_toolbar.rb
+++ lib/textile_toolbar.rb
@@ -1,10 +1,12 @@
 module TextileToolbar
   def textile_toolbar(id)
-    html = link_to_function(image_tag("textile_toolbar/bold.png", :size => "23x22", :alt => "Make selection bold"), "surround_selection('#{id}', '*')")
+    html = link_to_function(image_tag("textile_toolbar/bold.png", :size => "23x22", :alt => "Make selection bold"), "surround_selection('#{id}', '*', '*')")
     html << "&nbsp;"
-    html << link_to_function(image_tag("textile_toolbar/italic.png", :size => "23x22", :alt => "Make selection italic"), "surround_selection('#{id}', '_')")
+    html << link_to_function(image_tag("textile_toolbar/italic.png", :size => "23x22", :alt => "Make selection italic"), "surround_selection('#{id}', '_', '_')")
     html << "&nbsp;"
-    html << link_to("Textile", "http://hobix.com/textile/") << "&nbsp;enabled"
+    html << link_to_function(image_tag("textile_toolbar/hyperlink.png", :size => "23x22", :alt => "Make hyperlink"), "insert_hyperlink('#{id}')")
+    html << "&nbsp;&nbsp;"
+    html << "<small>" << link_to("Textile", "http://hobix.com/textile/") << "&nbsp;enabled</small>"
   end
 end
 
