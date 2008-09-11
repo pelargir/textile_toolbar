3725e40936afc2859f1198738fb05fdee07b1412
Matthew Bass <matt@anacreon.local>
2008-09-11 00:21:39 -0400

    added image insertion button

--- files/javascript/textile_toolbar.js
+++ files/javascript/textile_toolbar.js
@@ -15,7 +15,25 @@ function surround_selection(text_area, prefix, suffix) {
 	}
 }
 
+function insert_at_selection(text_area, value) {
+	text_area = $(text_area);
+	if (document.selection) { //IE support
+		text_area.focus();
+		document.selection.createRange().text = value;
+	} else if (text_area.selectionStart || text_area.selectionStart == '0') { //Mozilla/Firefox/Netscape 7+ support		
+		var startPos = text_area.selectionStart;
+		var endPos = text_area.selectionEnd;
+		text_area.value = text_area.value.substring(0, startPos) + value +
+										  text_area.value.substring(endPos, text_area.value.length);
+	}	
+}
+
 function insert_hyperlink(text_area) {
 	url = prompt("Enter URL for hyperlink:", "http://");
 	surround_selection(text_area, '"', '":' + url);
+}
+
+function insert_image(text_area) {
+	url = prompt("Enter URL for image:", "http://");
+	insert_at_selection(text_area, '!' + url + '!');
 }
--- lib/textile_toolbar.rb
+++ lib/textile_toolbar.rb
@@ -1,16 +1,21 @@
 module TextileToolbar
   def textile_area(object_name, method, options={})
-    toolbar = textile_toolbar(options[:id] || "#{object_name}_#{method}")
+    disable = options.delete(:disable) || {}
+    toolbar = textile_toolbar(options[:id] || "#{object_name}_#{method}", disable)
     toolbar + text_area(object_name, method, options)
   end
   
-  def textile_toolbar(id)
+  def textile_toolbar(id, disable)
     html = link_to_function(image_tag("textile_toolbar/bold.png", :size => "23x22", :alt => "Make selection bold"), "surround_selection('#{id}', '*', '*')")
     html << "&nbsp;"
     html << link_to_function(image_tag("textile_toolbar/italic.png", :size => "23x22", :alt => "Make selection italic"), "surround_selection('#{id}', '_', '_')")
     html << "&nbsp;"
     html << link_to_function(image_tag("textile_toolbar/hyperlink.png", :size => "23x22", :alt => "Make hyperlink"), "insert_hyperlink('#{id}')")
-    html << "&nbsp;&nbsp;"
+    html << "&nbsp;"
+    unless disable == :image
+      html << link_to_function(image_tag("textile_toolbar/image.png", :size => "23x22", :alt => "Insert image"), "insert_image('#{id}')")
+      html << "&nbsp;&nbsp;"
+    end
     html << "<small>" << link_to("Textile", "http://hobix.com/textile/", :target => "_blank") << "&nbsp;enabled</small>"
   end
 end
