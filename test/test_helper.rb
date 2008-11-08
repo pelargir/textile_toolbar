150f96d47cac729443de1707390dc6e76fd19049
Matthew Bass <matt@anacreon.local>
2008-11-08 09:45:59 -0500

    began adding tests

---
+++ test/test_helper.rb
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+require 'mocha'
+require 'action_controller'
+
+require File.expand_path(File.dirname(__FILE__) + '/../lib/textile_toolbar')
---
+++ test/textile_toolbar_test.rb
@@ -0,0 +1,18 @@
+require File.expand_path(File.dirname(__FILE__) + '/test_helper')
+
+class TextileToolbarTest < Test::Unit::TestCase
+  include TextileToolbar
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::AssetTagHelper
+  include ActionView::Helpers::JavaScriptHelper
+  include ActionView::Helpers::UrlHelper
+  include ActionView::Helpers::FormHelper
+  
+  context "textile_area" do
+    should "render text area" do
+      assert textile_area(:article, :body).include?("<textarea")
+      assert textile_area(:article, :body).include?("</textarea>")
+    end
+  end
+end
+
