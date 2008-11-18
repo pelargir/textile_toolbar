e448bfcd13e219bbc33d1446b0768cc1319f3c68
Matthew Bass <matt@anacreon.local>
2008-11-18 12:46:23 -0500

    added underlining button to toolbar

--- lib/textile_toolbar.rb
+++ lib/textile_toolbar.rb
@@ -7,9 +7,12 @@ module TextileToolbar
   end
   
   def textile_toolbar(id, disable)
-    html = link_to_function(image_tag("textile_toolbar/bold.png", :size => "23x22", :alt => "Make selection bold"), "surround_selection('#{id}', '*', '*')")
+    html = javascript_include_tag("textile_toolbar")
+    html << link_to_function(image_tag("textile_toolbar/bold.png", :size => "23x22", :alt => "Make selection bold"), "surround_selection('#{id}', '*', '*')")
     html << "&nbsp;"
     html << link_to_function(image_tag("textile_toolbar/italic.png", :size => "23x22", :alt => "Make selection italic"), "surround_selection('#{id}', '_', '_')")
+    html << "&nbsp;"
+    html << link_to_function(image_tag("textile_toolbar/underline.png", :size => "23x22", :alt => "Make selection underlined"), "surround_selection('#{id}', '+', '+')")
     html << "&nbsp;"
     html << link_to_function(image_tag("textile_toolbar/hyperlink.png", :size => "23x22", :alt => "Make hyperlink"), "insert_hyperlink('#{id}')")
     html << "&nbsp;"
