3d0b5dad8fe7b0a12681b32c4fd3eb34579d25ae
Matthew <pelargir@gmail.com>
2013-02-08 18:33:42 -0500

    made output from helper methods HTML safe

--- lib/textile_toolbar.rb
+++ lib/textile_toolbar.rb
@@ -4,24 +4,26 @@ module TextileToolbar
   def textile_area(object_name, method, options={})
     disable = options.delete(:disable) || {}
     toolbar = textile_toolbar(options[:id] || "#{object_name}_#{method}", disable)
-    raw("<div class=\"textile_toolbar\">#{toolbar}</div><div class=\"textile_area\">#{text_area(object_name, method, options)}</div>")
+    html = "<div class=\"textile_toolbar\">#{toolbar}</div><div class=\"textile_area\">#{text_area(object_name, method, options)}</div>"
+    html.html_safe
   end
   
   def textile_toolbar(id, disable)
     html = javascript_include_tag("textile_toolbar")
     html << link_to_function(image_tag("textile_toolbar/bold.png", :size => "23x22", :alt => "Make selection bold"), "surround_selection('#{id}', '*', '*')")
-    html << raw("&nbsp;")
+    html << "&nbsp;"
     html << link_to_function(image_tag("textile_toolbar/italic.png", :size => "23x22", :alt => "Make selection italic"), "surround_selection('#{id}', '_', '_')")
-    html << raw("&nbsp;")
+    html << "&nbsp;"
     html << link_to_function(image_tag("textile_toolbar/underline.png", :size => "23x22", :alt => "Make selection underlined"), "surround_selection('#{id}', '+', '+')")
-    html << raw("&nbsp;")
+    html << "&nbsp;"
     html << link_to_function(image_tag("textile_toolbar/hyperlink.png", :size => "23x22", :alt => "Make hyperlink"), "insert_hyperlink('#{id}')")
-    html << raw("&nbsp;")
+    html << "&nbsp;"
     unless disable == :image
       html << link_to_function(image_tag("textile_toolbar/image.png", :size => "23x22", :alt => "Insert image"), "insert_image('#{id}')")
-      html << raw("&nbsp;")
+      html << "&nbsp;"
     end
-    html << raw("<small>") << link_to("Textile", "http://hobix.com/textile/", :target => "_blank") << raw(" enabled</small>")
+    html << "<small>" << link_to("Textile", "http://hobix.com/textile/", :target => "_blank") << " enabled</small>"
+    html.html_safe
   end
   
   module FormBuilder
